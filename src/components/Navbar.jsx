9003e1bd22594f5edfe8ef7fe1f9ab3040c72fd1
Lucas Souza <lucasouza.doliveira@gmail.com>
2022-11-22 11:25:42 -0300

    Final project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+
+import './Navbar.css';
+
+const Navbar = () => {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
+    function handleSubmit(event) {
+        event.preventDefault(); 
+
+        if(!search) return null;
+        navigate(`/search?q=${search}`);
+        setSearch("");
+    }
+
+    return (
+        <nav id="navbar">
+            <h2>
+                <Link to="/">
+                    <BiCameraMovie />
+                    MoviesLib
+                </Link>
+            </h2> 
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Busque um filme" onChange={({target}) => setSearch(target.value)} value={search} />
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
+  )
+}
+
+export default Navbar
